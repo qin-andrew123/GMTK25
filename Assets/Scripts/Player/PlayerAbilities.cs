3969045bc609b9e2a9e8d8ba7f51f7fa72d6b693
Gia Lang <gia.arah@gmail.com>
2025-07-31 19:47:23 -0700

    added player trigger for glitchable objects and temp sprite

--- Assets/Scripts/Player/PlayerAbilities.cs
+++ Assets/Scripts/Player/PlayerAbilities.cs
@@ -4,11 +4,10 @@ using UnityEngine.Timeline;
 
 public class PlayerAbilities : MonoBehaviour
 {
-    private CharacterController mCharacterController;
+    private CapsuleCollider mCapsuleCollider;
 
-    [SerializeField]
-    private float mMaxGlitchRadius;
-    private float mAbilityCooldown;
+    [SerializeField] private float mMaxGlitchRadius;
+    [SerializeField] private float mAbilityCooldown;
     private bool bHasTriggeredAbility = false;
     
     
@@ -16,46 +15,50 @@ public class PlayerAbilities : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        mCharacterController = GetComponent<CharacterController>();
+        mCapsuleCollider = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float abilityInput = Input.GetAxisRaw("Ability1");
-        if((abilityInput >= 1.0f) && !bHasTriggeredAbility)
+        if(!bHasTriggeredAbility && (abilityInput >= 1.0f))
         {
-            Glitch();
+            Debug.Log("Triggered Ability");
+
             bHasTriggeredAbility = true;
+            Glitch();
         }
     }
 
     private void Glitch()
     {
-        // Check if in the Radius of a glitchable object via spherecast?
-        RaycastHit raycastHit;
-
-        Vector3 position = mCharacterController.center;
-        bool hit = Physics.SphereCast(position, mCharacterController.height / 2.0f, transform.forward, out raycastHit, mMaxGlitchRadius);
-        if(raycastHit.collider.CompareTag("GlitchableObject"))
+        Collider[] colliders = Physics.OverlapSphere(transform.position, (mCapsuleCollider.radius + mMaxGlitchRadius));
+        for(int i=0; i<colliders.Length; i++) 
         {
-            // Trigger success glitch
+            Collider collider = colliders[i];
+            if (collider.CompareTag("GlitchableObject"))
+            {
+                // Trigger success glitch
+                Debug.Log("Glitch!");
 
-            // Success glitch vfx
+                // Success glitch vfx
 
-            // Begin cooldown
-            StartCoroutine(AbilityCooldown());
-        }
-        else
-        {
-            // failure glitch vfx
+                // Begin cooldown
+                StartCoroutine(AbilityCooldown());
+                return;
+            }
         }
 
+        // failure glitch vfx
+        Debug.Log("No Glitch");
+        bHasTriggeredAbility = false;
     }
 
     private IEnumerator AbilityCooldown()
     {
         yield return new WaitForSeconds(mAbilityCooldown);
+        Debug.Log("End of Cooldown");
         bHasTriggeredAbility = false;
     }
 
