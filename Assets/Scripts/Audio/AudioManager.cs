154d91d886950e0a441258e0d84c23a41178d01c
Daniel Yu <danielyu742@outlook.com>
2025-08-01 20:47:25 -0700

    Split music into layers

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering;
 
+public enum AudioLevelState
+{
+    Code,
+    Game,
+    UI
+}
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
-    [SerializeField, Tooltip("Music track")]
+    [SerializeField]
     private AudioEvent mBGM;
     private AudioPlayer mAudioPlayer;
     [SerializeField, Tooltip("Glitch presets for each level")]
@@ -15,23 +21,58 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     private int mCurrentGlitchPreset = 0;
 
-    private AudioSource mBGMSource;
+    private AudioSource mCodeSource;
+    private AudioSource mUISource;
+    private AudioLevelState mAudioLevelState = AudioLevelState.Game;
 
     private void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
         mAudioPlayer = GetComponent<AudioPlayer>();
+        DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
     {
-        mBGMSource = transform.AddComponent<AudioSource>();
-        mBGMSource.resource = mBGM.mSound[0];
-        mBGMSource.volume = mBGM.mVolumeScale;
-        mBGMSource.loop = true;
-        mBGMSource.Play();
+        mCodeSource = transform.AddComponent<AudioSource>();
+        mCodeSource.resource = mBGM.mSound[0];
+        mCodeSource.loop = true;
+
+        mUISource = transform.AddComponent<AudioSource>();
+        mUISource.resource = mBGM.mSound[1];
+        mUISource.loop = true;
+
+        mCodeSource.Play();
+        mUISource.Play();
 
         StartGlitch();
+    }
+
+    public void SetAudioLevelState(AudioLevelState audioLevelState)
+    {
+        mAudioLevelState = audioLevelState;
+
+        switch (audioLevelState)
+        {
+            case AudioLevelState.Code:
+                mCodeSource.mute = false;
+                mUISource.mute = true;
+                break;
+            case AudioLevelState.Game:
+                mCodeSource.mute = false;
+                mUISource.mute = false;
+                break;
+            case AudioLevelState.UI:
+                mCodeSource.mute = true;
+                mUISource.mute = false;
+                break;
+        }
     }
 
     public void SetGlitchLevel(int level)
@@ -58,17 +99,21 @@ public class AudioManager : MonoBehaviour
                 float volume = Random.Range(0f, 1f / currentPreset.mGlitchIntensity);
                 volume = Mathf.Round(volume);
                 volume *= currentPreset.mGlitchIntensity;
-                mBGMSource.volume = volume;
+                mCodeSource.volume = volume;
+                mUISource.volume = volume;
 
-                mBGMSource.pitch = 1 + currentPreset.mPitchOffset;
+                mCodeSource.pitch = 1 + currentPreset.mPitchOffset;
+                mUISource.pitch = 1 + currentPreset.mPitchOffset;
 
                 float delay = Random.Range(currentPreset.mMinGlitchDelay, currentPreset.mMaxGlitchDelay);
                 yield return new WaitForSecondsRealtime(delay * currentPreset.DelayModifier);
             }
             else
             {
-                mBGMSource.volume = mBGM.mVolumeScale;
-                mBGMSource.pitch = 1f;
+                mCodeSource.volume = mBGM.mVolumeScale;
+                mUISource.volume = mBGM.mVolumeScale;
+                mCodeSource.pitch = 1f;
+                mUISource.pitch = 1f;
                 yield return null;
             }
         }
