f4a478fcae7ae03bc7b7634dd603a387a07b0a94
Gia Lang <gia.arah@gmail.com>
2025-08-01 19:26:20 -0700

    added spikes

---
+++ Assets/Scripts/GameObjects/Spike.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    [SerializeField]
+    private Transform respawnPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Respawn(other.gameObject);
+        }
+    }
+
+    private void Respawn(GameObject player)
+    {
+        player.transform.position = respawnPosition.position;
+        // TODO: play respawn glitch VFX
+    }
+}
